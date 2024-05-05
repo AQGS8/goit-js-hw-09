e38e28958a7d03a781fffe2362948cd1cc12ebf7
AQGS8 <gutek.48s+git@gmail.com>
2024-05-06 00:04:00 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,58 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const startBtn = document.querySelector('[data-start]');
 
-// function zmianaKoloruTla() {
-//     document.body.style.backgroundColor = getRandomHexColor();
-//     setTimeout(zmianaKoloruTla, 1000);
-// }
+const stopBtn = document.querySelector('[data-stop]');
+
+
+// document.body.style.backgroundColor = getRandomHexColor();
+
+function zmianaKoloruTla() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    setTimeout(zmianaKoloruTla, 1000);
+}
+
+startBtn.addEventListener('click', function(){
+    console.log("START")
+    this.disabled = true;
+    stopBtn.disabled = false;
+    zmianaKoloruTla();
+})
+
+stopBtn.addEventListener('click', function(){
+    console.log("STOP")
+    this.disabled = true;
+    startBtn.disabled = false;
+    clearTimeout(zmianaKoloruTla());
+})
+
+
+// let timeoutId;
+// timeoutId = setTimeout(changeBackgroundColor, 1000);
+//   return timeoutId;
+// clearTimeout(timeoutId);
+
+
+let t;
+ 
+function color() {
+    if (document.getElementById('btn'
+    ).style.color == 'blue') {
+        document.getElementById('btn')
+            .style.color = 'green';
+    } else {
+        document.getElementById('btn')
+            .style.color = 'blue';
+    }
+
+}
+
+function fun() {
+    t = setTimeout(color, 2000);
+
+}
+
+function stop() {
+    clearTimeout(t);
+}
