126a1af080534d744900c01b3331edac230c0156
AQGS8 <gutek.48s+git@gmail.com>
2024-05-07 19:06:43 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,30 +1,30 @@
 function getRandomHexColor() {  
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;    // funkcja losowego koloru
 }
 
-const startBtn = document.querySelector('[data-start]');
+const startBtn = document.querySelector('[data-start]');    // odwołanie do przycisku start
 
-const stopBtn = document.querySelector('[data-stop]');
+const stopBtn = document.querySelector('[data-stop]');      // odwołanie do przycisku stop
 
-let  zmianaKoloruTlaOFF;
+let  zmianaKoloruTlaOFF;          // stała do zapisania stanu funkcji
 
-function zmianaKoloruTla() {
+function zmianaKoloruTla() {                 // funkcja zmieniająca kolor tła 
     document.body.style.backgroundColor = getRandomHexColor();
     zmianaKoloruTlaOFF =  setTimeout(zmianaKoloruTla, 1000);
     return zmianaKoloruTlaOFF;
 }
 
 
-startBtn.addEventListener('click', function(){
-    console.log("START")
-    this.disabled = true;
-    stopBtn.disabled = false;
-    zmianaKoloruTla();
+startBtn.addEventListener('click', function(){         // funkcjonalność przycisku start 
+    console.log("START")                               // wypisuje w konsoli start, zeby łatwiej śledzić ilość kliknięć
+    this.disabled = true;                            // zmienia przycisk start na nieaktywny
+    stopBtn.disabled = false;                        // zmienia przycisk stop na aktywny
+    zmianaKoloruTla();                                // uruchamia funkcje zmiany koloru
 })
 
-stopBtn.addEventListener('click', function(){
-    console.log("STOP")
-    this.disabled = true;
-    startBtn.disabled = false;
-    clearTimeout(zmianaKoloruTlaOFF);
+stopBtn.addEventListener('click', function(){           // funkcjonalność przycisku stop
+    console.log("STOP")                                 
+    this.disabled = true;                           // zmiania przycisk stop na nieaktywny
+    startBtn.disabled = false;                       // zmienia przycisk start na aktywny
+    clearTimeout(zmianaKoloruTlaOFF);                // zatrzumuje funkcję zmiany koloru 
 })
